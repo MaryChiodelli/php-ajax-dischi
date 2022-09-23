113cf607d70a891bf764ae502b136161d633d2c2
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-23 15:52:39 +0200

    print dischi with php

--- prima-versione/index.php
+++ prima-versione/index.php
@@ -5,11 +5,45 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP dischi</title>
+  <link rel="stylesheet" href="../style.css">
 </head>
 <body>
   <?php
   include __DIR__ . '/../dischi.php';
-  var_dump($dischi);
   ?>
+
+  <header>
+    <img class="header-logo" src="../logo.png" alt="Spotify logo">
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="row">
+
+        <?php
+        foreach($dischi as $disco) {
+        ?>
+        <div class="col">
+          <div class="card">
+            <img class="card-img" src="<?= $disco['poster'] ?>" alt="<?= $disco['title'] ?>">
+            <h3 class="card-title">
+              <?= $disco['title'] ?>
+            </h3>
+            <div class="card-author">
+              <?= $disco['author'] ?>
+            </div>
+            <div class="card-year">
+              <?= $disco['year'] ?>
+            </div>
+          </div><!--/card -->
+        </div><!--/col -->
+        <?php
+        }
+        ?>
+        
+      </div><!--/row -->
+    </div><!--/container -->
+  </main>
+
 </body>
 </html>
